6d9cb01035f06fe1389286a33dab706dcf4e329a
SilverMaiden <h.specials@gmail.com>
2019-10-11 10:00:25 -0700

    completed MVP

--- Answers.md
+++ Answers.md
@@ -2,10 +2,22 @@
 
 1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+Because of it’s ability to handle and update based on state, it solves the problem of how to 
+have a smooth running app that evolves with its changing data.
+
 1. What does it mean to think in react?
+
+It means thinking declaratively.
 
 1. Describe state.
 
+State in React is just like state in anything else. It can go from being in one state to another, and having certain
+things in the app change when the state changes.
+
 1. Describe props.
 
+Props are a way to pass data down to child components.
+
 1. What are side effects, and how do you sync effects in a React component to state or prop changes?
+
+You can use useEffect.
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CharacterCardHolder from './components/CardHolder/CardHolder';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [peopleList, setPeopleList] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+    .then(response => {
+        //1: get response.data.results
+        //2: map over the array, and push each item to peopleList
+        setPeopleList(response.data.results);
+
+
+    })
+
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharacterCardHolder peopleList={peopleList} />
+
     </div>
   );
 }
